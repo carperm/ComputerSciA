60dbf8f44274e52b7964ff82688acd4e5f2aa2da
carperm <159586291+carperm@users.noreply.github.com>
2025-02-13 14:10:33 -0500

    Create MyProgram.java

---
+++ MyProgram.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MyProgram
+{        
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new GUIApp());
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
