71ecc5e063b5fbc3458de7641f550d714ca573bb
carperm <159586291+carperm@users.noreply.github.com>
2025-03-13 11:51:49 -0400

    Create Square.java

---
+++ GUIShapes/Square.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class Square extends Rectangle {
+    
+    public Square(Color c, Point p, int width) {
+        super(c,p,width,width);
+    }
+}
